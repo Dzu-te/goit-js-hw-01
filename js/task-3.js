ac1df15f538eb8ad7927d7cfc63cea28412c26ac
Dzu-te <108475129+Dzu-te@users.noreply.github.com>
2023-10-28 15:25:01 +0300

    add task-3

--- js/task-3.js
+++ js/task-3.js
@@ -12,9 +12,15 @@
 // console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 // console.log(getElementWidth("200px", "0px", "0px")); // 200
 
-function getElementWidth(content, padding, border) {
-    
+function convertToDecimal(value) {
+    return Number.parseFloat(value.replace("," , "."))
+}
 
+function getElementWidth(content, padding, border) {
+    content = convertToDecimal(content);
+    padding = convertToDecimal(padding);
+    border = convertToDecimal(border);
+    return content + padding * 2 + border * 2;
 }
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
